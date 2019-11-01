ec03cfde0f98be07a9ef2279d6fca3b6777b17fd
yawar1101 <mohdyawars@gmail.com>
2019-11-01 18:28:52 +0530

    testing ssh

--- polls/models.py
+++ polls/models.py
@@ -20,5 +20,6 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+
     def __str__(self):
         return self.choice_text
